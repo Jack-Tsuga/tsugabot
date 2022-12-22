6e65513c3e1800141ab9ab7bfa3dae8728285146
Jack-Tsuga <101634529+Jack-Tsuga@users.noreply.github.com>
2022-12-22 16:54:57 +0900

    line bot add 1

--- index.py
+++ index.py
@@ -40,7 +40,7 @@ LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 # 環境変数からLINE Channel Secretを設定
 LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-#line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 #handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/")
