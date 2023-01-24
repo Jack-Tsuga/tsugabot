c3a814743bcb34be925b0d8dd096aa75fecac240
Jack-Tsuga <101634529+Jack-Tsuga@users.noreply.github.com>
2023-01-24 19:26:54 +0900

    files 5

--- index.py
+++ index.py
@@ -258,8 +258,8 @@ def productlist():
 			{"id": 234,"image_b64data":image_b64data[11],"name": 'ショコラ', "yomi": "しょおこら", "shop": "和将ケーキ店", "price": "270", "stock": "40"},
 			{"id": 678,"image_b64data":image_b64data[12],"name": 'モンブラン', "yomi": "もんぶらん", "shop": "高丘菓子店", "price": "220", "stock": "23"}]
 
-	return "<h1> test </h1>"+','.join(files) 
-	#return render_template("/mngmt/productlist.html",products=items)
+	#return "<h1> test </h1>"+','.join(files) 
+	return render_template("/mngmt/productlist.html",products=items)
 
 @app.route("/mngmt/product_resp", methods=['POST'])
 def product_resp():
